52f0d10919274e96e03e36a982ac1e11e9c3dbc5
ArloFyor <richarduy.business@gmail.com>
2023-12-06 14:33:53 +0800

    Initial Commit

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View>
+      <Text>Hello World</Text>
     </View>
-  );
+  )
 }
+
+export default App

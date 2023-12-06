a323051ad8338e4ee7ebc0324def1337c652ce3d
ArloFyor <richarduy.business@gmail.com>
2023-12-06 21:45:57 +0800

    Added Post Footer Assets.

--- components/home/Post.js
+++ components/home/Post.js
@@ -1,28 +1,41 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
-import { Divider } from 'react-native-elements'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import { Divider } from 'react-native-elements';
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   return (
-    <View style={{marginBottom: 20, marginTop: 8}}>
-      <Divider width={1} orientation='vertical'/>
+    <View style={{ marginBottom: 20, marginTop: 8 }}>
+      <Divider width={1} orientation='vertical' />
       <PostHeader post={post} />
-    </View>
-  )
-}
-
-const PostHeader = ({post}) => {
-  return (
-    <View style={{flexDirection: 'row', justifyContent: 'space-between', margin: 5, alignItems: 'center'}}>
-      <View style = {{ flexDirection: 'row', alignItems: 'center' }}>
-        <Image source={post.profile_picture} style={styles.profilePicture} />
-        <Text style={{ marginLeft: 6, fontWeight: 600, fontSize: 15, marginTop: 4 }}>{post.user}</Text>
-      </View>
+      <PostImage post={post} />
     </View>
   );
-}
+};
 
-export default Post
+const PostHeader = ({ post }) => {
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-between', margin: 5, alignItems: 'center' }}>
+      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+        <Image source={post.profile_picture} style={styles.profilePicture} />
+        <Text style={{ marginLeft: 8, fontSize: 15, fontFamily: 'serif', marginTop: 5 }}>{post.user}</Text>
+      </View>
+      <Text style={{ fontWeight: 'bold', marginRight: 10 }}>...</Text>
+    </View>
+  );
+};
+
+const PostImage = ({ post }) => {
+  return (
+    <View style={{ width: '100%', height: 450, alignItems: 'center', justifyContent: 'center' }}>
+      <Image
+        source={post.imageURL}
+        style={{ height: '100%', width: '100%', resizeMode: 'cover', aspectRatio: 0.9 }}
+      />
+    </View>
+  );
+};
+
+export default Post;
 
 const styles = StyleSheet.create({
   profilePicture: {
@@ -30,7 +43,7 @@ const styles = StyleSheet.create({
     height: 40,
     marginLeft: 10,
     marginTop: 10,
-    borderRadius: 50, // to make it a circular image
+    borderRadius: 20, // to make it a circular image (half of width and height)
     borderWidth: 1.6,
     borderColor: '#FCC153',
   },

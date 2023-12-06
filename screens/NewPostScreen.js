8ad60b5f4ed680c8c6ce616ae91f10b97161c4c9
ArloFyor <richarduy.business@gmail.com>
2023-12-07 03:48:11 +0800

    Updated NewPostScreen.js Currently using console.log on handlePost

--- screens/NewPostScreen.js
+++ screens/NewPostScreen.js
@@ -1,47 +1,49 @@
-import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import React, { useState } from 'react';
-import ImagePicker from 'react-native-image-picker';
+import { View, Text, Image, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const NewPostScreen = () => {
   const [caption, setCaption] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleImageSelection = () => {
-    const options = {
-      title: 'Select Image',
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-
-    // Use launchImageLibrary instead of showImagePicker
-    ImagePicker.launchImageLibrary(options, (response) => {
-      if (response.didCancel) {
-        console.log('Image selection canceled');
-      } else if (response.error) {
-        console.error('ImagePicker Error:', response.error);
-      } else {
-        setSelectedImage(response.uri);
-      }
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
+
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      setSelectedImage(result.assets[0].uri);
+    }
   };
 
   const handlePost = () => {
     console.log('Caption:', caption);
     console.log('Selected Image:', selectedImage);
-    // Add logic to send the data to your server or perform other actions.
+
+    // Add logic to send data to your server or perform other actions.
+
+    // Clear state values
+    setCaption('');
+    setSelectedImage(null);
   };
 
   return (
     <View style={styles.container}>
+      {/* Button positioned at the top left */}
+      <TouchableOpacity style={styles.homeButtonContainer} onPress={() => console.log('Home Button Pressed')}>
+        <Image style={styles.homeButton} source={require('../assets/Buttons/Home_Button.png')} />
+      </TouchableOpacity>
+
       <Text style={styles.title}>New Post</Text>
 
       {selectedImage && (
         <Image source={{ uri: selectedImage }} style={styles.selectedImage} />
       )}
 
-      <TouchableOpacity onPress={handleImageSelection}>
+      <TouchableOpacity onPress={pickImage}>
         <Text style={styles.selectImageText}>Select Image</Text>
       </TouchableOpacity>
 
@@ -60,44 +62,54 @@ const NewPostScreen = () => {
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 16,
-      },
-      selectedImage: {
-        width: 200,
-        height: 200,
-        borderRadius: 8,
-        marginBottom: 16,
-      },
-      selectImageText: {
-        color: 'blue',
-        fontSize: 16,
-        marginBottom: 16,
-      },
-      captionInput: {
-        borderWidth: 1,
-        borderColor: 'gray',
-        borderRadius: 8,
-        padding: 8,
-        marginBottom: 16,
-        width: '100%',
-      },
-      postButton: {
-        color: 'white',
-        backgroundColor: 'blue',
-        padding: 12,
-        borderRadius: 8,
-        textAlign: 'center',
-        fontSize: 18,
-      },
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5DC',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  selectedImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  selectImageText: {
+    color: 'blue',
+    fontSize: 16,
+    marginBottom: 16,
+  },
+  captionInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 8,
+    padding: 8,
+    marginBottom: 16,
+    width: '100%',
+  },
+  postButton: {
+    color: 'white',
+    backgroundColor: 'blue',
+    padding: 12,
+    borderRadius: 8,
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  homeButtonContainer: {
+    alignSelf: 'flex-start',
+    bottom: 100
+  },
+  
+  homeButton: {
+    height: 80,
+    width: 80,
+  },
 });
 
 export default NewPostScreen;

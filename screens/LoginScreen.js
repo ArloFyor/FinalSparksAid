9f33359d9b6573b54553055cbfcfa1a05cae5f77
ArloFyor <richarduy.business@gmail.com>
2023-12-08 00:41:35 +0800

    Updated LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,78 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet, ImageBackground } from 'react-native';
 
-const LoginScreen = () => {
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Implement your login logic here
+    // For simplicity, let's just log the entered username and password
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  )
-}
+    <ImageBackground source={require('../assets/Backgrounds/LoginAndRegistrationBackground.png')} style={styles.background}>
+      <View style={styles.container}>
+        <Text style={styles.logo}>SparksAid</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          onChangeText={text => setUsername(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          onChangeText={text => setPassword(text)}
+        />
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
+  );
+};
 
-export default LoginScreen
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.3)', // Semi-transparent background for better readability
+  },
+  logo: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: 'white',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    marginBottom: 10,
+    paddingLeft: 10,
+    backgroundColor: 'white', // Input background color
+  },
+  loginButton: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
 
-const styles = StyleSheet.create({})
+export default App;

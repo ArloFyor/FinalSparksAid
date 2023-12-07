f2c864da8d6e91885a8f06a9497ab012776688f3
ArloFyor <richarduy.business@gmail.com>
2023-12-07 23:02:41 +0800

    Updated App.js for navigation.

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import HomeScreen from './screens/HomeScreen'
-import NewPostScreen from './screens/NewPostScreen'
+import SignedInStack from './navigation'
 
 const App = () => {
   return (
-    <NewPostScreen/>
+    <SignedInStack />
   )
 }
 

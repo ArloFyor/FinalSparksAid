6d998c450e1aadef70e5e54a7dd133726f49941e
ArloFyor <richarduy.business@gmail.com>
2023-12-09 15:29:23 +0800

    Added initial RegistrationScreenOne.js

---
+++ screens/RegistrationScreenOne.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RegistrationScreenOne = () => {
+  return (
+    <View>
+      <Text>RegistrationScreenOne</Text>
+    </View>
+  )
+}
+
+export default RegistrationScreenOne

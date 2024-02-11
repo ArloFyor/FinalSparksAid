c611aef1fe8e77c57d63bfe501a083a979ca9c03
ArloFyor <richarduy.business@gmail.com>
2024-02-11 23:25:39 +0800

    Updated Packages and Removed DropDown Picker for Initial Building

--- components/registration/registrationOne/RegistrationFieldsOne.js
+++ components/registration/registrationOne/RegistrationFieldsOne.js
@@ -4,13 +4,13 @@ import DropDownPicker from 'react-native-dropdown-picker';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 
+
 const registrationSchema = yup.object().shape({
-  userType: yup.string().oneOf(['Caregiver', 'Patient', 'Guardian'], 'Invalid User Type').required('Required'),
   firstName: yup.string().required('Required'),
   lastName: yup.string().required('Required'),
 });
 
-const onRegister = async (userType, firstName, lastName) => {
+const onRegister = (userType, firstName, lastName) => {
   try {
     console.log('User account has been created: ', userType, firstName, lastName);
   } catch (error) {
@@ -18,21 +18,11 @@ const onRegister = async (userType, firstName, lastName) => {
   }
 };
 
-const RegistrationFieldsOne = ({ navigation }) => {
-
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(""); // Initialize with an empty string
-  const [items, setItems] = useState([
-    { label: 'Select an option.', value: '' },
-    { label: 'Caregiver', value: 'Caregiver' },
-    { label: 'Patient', value: 'Patient' },
-    { label: 'Guardian', value: 'Guardian' },
-  ]);
-
+const RegistrationFieldsOne = () => {
   return (
     <View>
       <Formik
-        initialValues={{ userType: "", firstName: "", lastName: "", }}
+        initialValues={{ firstName: "", lastName: "", }}
         onSubmit={(values, actions) => {
           onRegister(values.userType, values.firstName, values.lastName);
           actions.resetForm();
@@ -41,61 +31,49 @@ const RegistrationFieldsOne = ({ navigation }) => {
         validateOnMount={true}
       >
 
-        {({ handleBlur, handleChange, handleSubmit, isValid, values, errors, setFieldValue }) => (
-          <>
-            <View>
-              <DropDownPicker
-                open={open}
-                value={value} // Set the value directly from the state
-                items={items}
-                setOpen={setOpen}
-                setValue={(selectedValue) => {
-                  setValue(selectedValue);
-                  setFieldValue('userType', selectedValue); // Update userType in Formik
-                }}
-                setItems={setItems}
-                style={styles.picker}
-                zIndex={1000}
-              />
-            </View>
+      {({ handleBlur, handleChange, handleSubmit, isValid, values, errors, setFieldValue }) => (
+        <>
+          <View style={styles.input}>
+            <TextInput
+              placeholderTextColor="#444"
+              placeholder="First Name"
+              autoCapitalize="none"
+              textContentType="username"
+              onChangeText={(text) => handleChange('firstName')(text)}
+              onBlur={handleBlur('firstName')}
+              value={values.firstName}
+            />
+          </View>
 
-            <View style={styles.input}>
-              <TextInput
-                placeholderTextColor="#444"
-                placeholder="First Name"
-                autoCapitalize="none"
-                textContentType="username"
-                onChangeText={(text) => handleChange('firstName')(text)}
-                onBlur={handleBlur('firstName')}
-                value={values.firstName}
-              />
-            </View>
+          <View style={styles.input}>
+            <TextInput
+              placeholderTextColor="#444"
+              placeholder="Last Name"
+              autoCapitalize="none"
+              textContentType="username"
+              onChangeText={(text) => handleChange('lastName')(text)}
+              onBlur={handleBlur('lastName')}
+              value={values.lastName}
+            />
+          </View>
 
-            <View style={styles.input}>
-              <TextInput
-                placeholderTextColor="#444"
-                placeholder="Last Name"
-                autoCapitalize="none"
-                textContentType="username"
-                onChangeText={(text) => handleChange('lastName')(text)}
-                onBlur={handleBlur('lastName')}
-                value={values.lastName}
-              />
-            </View>
-
-            <TouchableOpacity
-              style={[styles.proceedButton, !isValid && styles.disabledProceedButton]}
-              onPress={handleSubmit}
-              disabled={!isValid}
+          <TouchableOpacity
+                //style={styles.proceedButton}
+                style={[styles.proceedButton, !isValid ? styles.disabledProceedButton : null]}
+                onPress={handleSubmit}
+                disabled={!isValid}
             >
-              <Image style={styles.proceedImage} source={require('../../../assets/LoginAndRegistrationAssets/proceedButton.png')} />
+                <Image style={styles.proceedImage} source={require('../../../assets/LoginAndRegistrationAssets/proceedButton.png')} />
             </TouchableOpacity>
-          </>
-        )}
+        </>
+      )}
+        
       </Formik>
     </View>
-  );
-};
+  )
+}
+
+export default RegistrationFieldsOne
 
 const styles = StyleSheet.create({
   input: {
@@ -135,5 +113,3 @@ const styles = StyleSheet.create({
     opacity: 0.5,
   },
 });
-
-export default RegistrationFieldsOne;

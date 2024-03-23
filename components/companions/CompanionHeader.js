711e4e194a80f49e229f61c2824cf58c7d4c959a
ArloFyor <richarduy.business@gmail.com>
2024-03-24 03:03:11 +0800

    Modified the size and positioning of the buttons in CompanionScreen

--- components/companions/CompanionHeader.js
+++ components/companions/CompanionHeader.js
@@ -5,7 +5,7 @@ const CompanionHeader = ({navigation}) => {
   return (
     <View style={styles.container}>
       
-        <TouchableOpacity style={styles.homeButtonOpacity} onPress={() => navigation.push('HomeScreen')}>
+        <TouchableOpacity onPress={() => navigation.push('HomeScreen')}>
             <Image 
             style={styles.homeButton} 
             source={require('../../assets/Buttons/whiteHomeButton.png')} 
@@ -14,9 +14,9 @@ const CompanionHeader = ({navigation}) => {
       
         <Text style={styles.companionText}>My <Text style={{color: '#FFBD59'}}>Companions</Text></Text>
 
-        <TouchableOpacity style={styles.homeButtonOpacity} onPress={() => navigation.push('AddCompanionScreen')}>
+        <TouchableOpacity onPress={() => navigation.push('AddCompanionScreen')}>
             <Image 
-            style={styles.homeButton} 
+            style={styles.addButton} 
             source={require('../../assets/Buttons/addCompanionButton.png')} 
             />
         </TouchableOpacity>
@@ -30,38 +30,23 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: '#6237CF',
         height: 130,
-        justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
       },
-    
-      logo: {
-          width: '60%',
-          height: '80%',
-          resizeMode: 'contain',
-          marginTop: 55,
-          marginLeft: 5,
-          marginRight: 5,
-      },
-    
-      homeButtonOpacity: {
-        alignSelf: 'flex-start',
-      },
       homeButton: {
-        width: 70,
-        height: 70,
-        marginTop: 55,
+        width: 60,
+        height: 60,
+        marginTop: 50,
+        marginLeft: 20,
+      },
+      addButton: {
+        width: 60,
+        height: 60,
+        marginTop: 50,
       },
       companionText: {
         marginTop: 55,
         fontSize: 32,
         color: '#FFFDE7',
-        marginRight: 0,
-      },
-
-      coloredLink:{
-        marginTop: 55,
-        textDecorationLine: 'underline',
-        color: '#FFBD59',
       },
 })

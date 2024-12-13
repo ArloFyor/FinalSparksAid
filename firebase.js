853e5f314a9c72c4956505428e12b894fc10c734
ArloFyor <richarduy.business@gmail.com>
2024-12-13 16:01:51 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,16 +6,6 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { getStorage, ref } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDmtoAqCGjhqonn1wMjU_Mu5iBe9nayGBI",
-  authDomain: "rn-sparksaid.firebaseapp.com",
-  projectId: "rn-sparksaid",
-  storageBucket: "rn-sparksaid.appspot.com",
-  messagingSenderId: "669640594251",
-  appId: "1:669640594251:web:7a40562dae90a869cccece",
-  measurementId: "G-1GHT7HXY70"
-};
-
 // Initialize Firebase with persistence
 const app = (getApps().length === 0 ? initializeApp(firebaseConfig) : getApp());
 const auth = initializeAuth(app, {
